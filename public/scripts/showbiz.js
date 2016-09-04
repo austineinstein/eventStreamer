337e889f1e24710475697a72887cea372302ce31
austineinstein <austin_agbo@yahoo.com>
2016-09-04 11:02:23 +0100

    Update showbiz.js

--- public/scripts/showbiz.js
+++ public/scripts/showbiz.js
@@ -27,16 +27,12 @@ function success_handler(position) {
 }
 
 function refresher(){
-	//collection = undefined;
 	event();
 } 
 
 //either do events inside angular, or angular inside events - 
 //so if wow == true then next button is active
 //my global variable is set to something and there are events to click through 
-
-
-
 function event() 
 {
 //https://robots.thoughtbot.com/buttons-with-hold-events-in-angularjs
@@ -49,7 +45,6 @@ var oArgs = {
   where: encodeURI(String(latlon.latitude)+","+String(latlon.longitude)),
   within: String(within),
   category: String(categoryid),
-  //performers: String(performers),
   venue_name: String(venue),
   page_size: String(pgsize)
 };
@@ -58,13 +53,9 @@ checker = EVDB.API.call("/events/search", oArgs, function(oData)
 	//console.log(oData);
     collection = oData.events.event;
 	$.each(collection, function(index, value) {
-		//(index + ":" + value); 
 		$("#venue").append("<option>"+ (value.venue_name) + "</option>");
 		
 	});
-	//Do a venue one with the jquery ALL to get venue!
-	
-	//$("#shower").append("<li>"+ (oData) + "</li>");
 	event_handler(); //hold the values //because we are in API Execution context  - GEt DAta!
 	if (checker == true)
 	{
@@ -94,9 +85,6 @@ function event_handler()
 angular.module('eventsApp', []) 
   .controller('MainCtrl', function($scope) {
 	  //MAke sure to hide button until Data is displayed
-	  //Try Display Image Object, and it should be shared to facebook
-	  //Facebook profilevpicture when logged in
-	  //$scope.submitData = event();
 	  $scope.submitData = function(){
 		  var self = this; 
 		  var i = 0;
@@ -104,12 +92,6 @@ angular.module('eventsApp', [])
 		  var j = 0;
 		  var phide;
 		  var nhide;
-		  //a[j] = collection[j];
-		  //a.push(collection);
-		  //console.log(a);
-		  //j = [++j]; 
-		  //Control so it does not display nulls or nothing
-		  //Clean so it arranges it in expected proper html format or just delete rubbish
 		  self.events = collection;
 		  
 		  //j = j + 1;
@@ -120,31 +102,9 @@ angular.module('eventsApp', [])
 		  };
 		  self.nextEvent = function() {
 			  self.event = self.events[++i];
-			  //if (collection.length === self.events[i])
-			  //{
-			//	  nhide = true;
-			 // }
 		  };
-		  
-		  //while (0 === i - 1)
-		  //{
-		//	  phide = true;
-		 // }
-		  //need to add prev & control it to so no next at the last event
-		  //Display length and how many more descriptions you have and next disappears when its the last one!
-		  //Previous is not present for the first one. 	  
 	  };
   });
-
-	//holder.hold would contain string
-	//to get concert location with events API
-  //var location = document.getElementById("userInput").value; 
-
-  //var search = encodeURI(string+location);
-  //document.getElementById("result").innerHTML = "You have entered " ;//+ location;
-  //able to click more for more information in lists
-  //able to specify type of sorting wanted for information
- 
 function failure_handler(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
